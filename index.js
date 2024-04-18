76a86ca8927d814166044945efceb6c71dca0e73
saranggit <sarangbkn2nit@gmail.com>
2024-04-18 17:38:23 +0530

    final

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ async function main() {
   const express = require("express");
 
   const bot = new Telegraf(process.env.BOT_TOKEN);
-  const diskwalaURL = "www.diskwala.com"
+  const diskwalaTelegram = "https://t.me/+zFtbyC6Sf3hiM2M1"
+  const diskwalaURL = "https://www.diskwala.com"
   bot.start(async (ctx) => {
     try {
       ctx.replyWithMarkdown(
-        `Hi ${ctx.message.from.first_name},\n\nI can create Downloadable Links from Terabox Link.\n\nMade with ❤️ by [Diskwala](${diskwalaURL})\n\nSend any terabox link to get downloadable link which you can upload to Diskwala Remote URL upload.`,
+        `Hi ${ctx.message.from.first_name},\n\nI can create Downloadable Links from Terabox Link.\n\nMade with ❤️ by [Diskwala](${diskwalaTelegram})\n\nSend any terabox link to get downloadable link which you can upload to Diskwala Remote URL upload.`,
       );
     } catch (e) {
       console.error(e);
@@ -26,13 +27,13 @@ async function main() {
         //const parts = messageText.split("/");
         //const linkID = parts[parts.length - 1];
 
-        // ctx.reply(linkID)
+        ctx.reply("Fetching Link Data...")
 
         const details = await getDetails(messageText);
         if (details && details.direct_link) {
           try {
-            ctx.reply(`Here is the downloadable link: `, details.direct_link);
-            ctx.replyWithMarkdown(`Copy this URL and paste it in Remote URL in Diskwala [Dashboard](${diskwalaURL})`)
+            ctx.reply(`Here is the downloadable link: \n${details.direct_link}`);
+            ctx.replyWithMarkdown(`Copy this URL and paste it in Remote URL in [Diskwala Dashboard](${diskwalaURL})`)
           } catch (e) {
             console.error(e); // Log the error for debugging
           }
