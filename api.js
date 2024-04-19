67b0ef6d1848175ef3b20d282ab4be43c2f3fe6c
saranggit <sarangbkn2nit@gmail.com>
2024-04-19 15:58:36 +0530

    console

--- api.js
+++ api.js
@@ -5,7 +5,9 @@ async function getDetails(id) {
         const response = await axios.get(
             `https://terabox.hnn.workers.dev/api/get-info?shorturl=5naGERVYJuIrtUZ9Ew8ZWw&pwd=?data=${id.split("url=")[1]}`
         );
+        console.log(response)
         const linkData = await axios.post("https://terabox.hnn.workers.dev/api/get-download",{"shareid":response.data.sharid,"uk":response.data.uk,"sign":response.data.sign,"timestamp":response.data.time,"fs_id": response.data.list[0].fs_id})
+        console.log(linkData)
         return linkData.data;
     } catch (error) {
         console.error(error);
