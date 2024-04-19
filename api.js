161af1667066820e10b98f171e05b2a5b1e19a0c
saranggit <sarangbkn2nit@gmail.com>
2024-04-19 15:45:52 +0530

    error

--- api.js
+++ api.js
@@ -5,7 +5,7 @@ async function getDetails(id) {
         const response = await axios.get(
             `https://terabox.hnn.workers.dev/api/get-info?shorturl=5naGERVYJuIrtUZ9Ew8ZWw&pwd=?data=${id.split("url=")[1]}`
         );
-        const linkData = const await axios.post("https://terabox.hnn.workers.dev/api/get-download", [...response.data, "fs_id": response.data.list[0].fs_id])
+        const linkData = await axios.post("https://terabox.hnn.workers.dev/api/get-download", [...response.data, "fs_id": response.data.list[0].fs_id])
         return linkData.data;
     } catch (error) {
         console.error(error);
