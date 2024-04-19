50136c3c4f455188084cee54cf3c808092efa907
saranggit <sarangbkn2nit@gmail.com>
2024-04-19 18:17:49 +0530

    new aws api

--- api.js
+++ api.js
@@ -4,7 +4,7 @@ async function getDetails(id) {
     console.log(id)
     try {
         const response = await axios.get(
-            `https://terabox-app-f8et.onrender.com/api?data=${id}`
+            `http://ec2-3-108-22-254.ap-south-1.compute.amazonaws.com/api?data=${id}`
         );
         console.log(response)
         //const linkData = await axios.post("https://terabox.hnn.workers.dev/api/get-download",{"shareid":response.data.shareid,"uk":response.data.uk,"sign":response.data.sign,"timestamp":response.data.timestamp,"fs_id": response.data.list[0].fs_id})
