76af58bc25fd88b51431b1b72c9cc2ec8cf0c24a
saranggit <sarangbkn2nit@gmail.com>
2024-04-20 08:08:39 +0530

    api changed

--- api.js
+++ api.js
@@ -4,7 +4,7 @@ async function getDetails(id) {
     console.log(id)
     try {
         const response = await axios.get(
-            `http://ec2-3-108-22-254.ap-south-1.compute.amazonaws.com/api?data=${id}`
+            `https://thewebtool.net/api?data=${id}`
         );
        
         return response.data;
